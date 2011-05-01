ece86923345241e398f9aad772db14df076b319b
Reason <reason@exratione.com>
2011-05-01 01:35:38 +0000

    tinkering with architecture, still undecided about a lot of things
    Signed-off-by: Reason <reason@exratione.com>

--- example/simpleExample.js
+++ example/simpleExample.js
@@ -1,8 +1,11 @@
 /**
- * An example of a thywill HTTP server, using a very simple configuration.
+ * An example of a thywill HTTP server, using a very simple configuration and a proof of concept application.
+ * All this does is echo messages submitted back to the client.
  */
 
-var thywill = require("thywill.js");
+var thywill = require("thywill");
+var echoApplicationClass = require("../apps/echo/echo");
+var echoApplication = new echoApplicationClass("my echo application");
 
 /*
  * Start up an HTTP server.
@@ -22,15 +25,8 @@ thywill.configureFromFile();
  * with an argument that is a reference to the thywill object for convenience, or we can listen for the 
  * "thywill.ready" event elsewhere in our code. Here we are passing a callback.
  */
-thywill.startup(function(thywill) {
-  
-
-  //TODO now add a small app: listener plus static paths on the http server
-  
-  
-  
-  
-  
+thywill.startup(echoApplication, function(thywill) {
+  console.log("thywill.js is ready.");
 });
 
 
