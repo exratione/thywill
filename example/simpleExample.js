d2080443b1b0c5b2f1527cc8f8551e05827507d8
Reason <reason@exratione.com>
2011-05-08 23:33:05 +0000

    untested fleshing out of client interface and attached stuff; added extra classes, changed signatures, etc
    Signed-off-by: Reason <reason@exratione.com>

--- example/simpleExample.js
+++ example/simpleExample.js
@@ -3,18 +3,29 @@
  * All this does is echo messages submitted back to the client.
  */
 
+var http = require("http");
 var thywill = require("thywill");
-var echoApplicationClass = require("../apps/echo/echo");
-var echoApplication = new echoApplicationClass("my echo application");
+var echoAppClass = require("../apps/echo");
 
 /*
  * Start up an HTTP server.
  */ 
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, { "Content-Type": "text/html"}); 
+  res.end("Echo Test");   
+});
 
+/*
+ * Then define your application object and callback function:
+ */
+var echoApp = new echoAppClass("my echo application");
+var callback = function() { 
+  console.log("thywill.js is ready.");
+};
 
 /*
  * Use the default configuration file in /config/thywill.json. Configuration methods
- * for thywill are synchronous, even when reading from file.
+ * for thywill are synchronous, even when reading from a file.
  */ 
 thywill.configureFromFile();
 
@@ -25,8 +36,6 @@ thywill.configureFromFile();
  * with an argument that is a reference to the thywill object for convenience, or we can listen for the 
  * "thywill.ready" event elsewhere in our code. Here we are passing a callback.
  */
-thywill.startup(echoApplication, function(thywill) {
-  console.log("thywill.js is ready.");
-});
+thywill.startup(server, echoApp, callback);
 
 
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require("./lib/thywill.js");
+module.exports = require("./lib/thywill");
