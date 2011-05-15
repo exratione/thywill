d890566564a52572eeba7b5996e2ed15c221bda5
Reason <reason@exratione.com>
2011-05-15 19:25:19 +0000

    much reordering and moving synchronous calls to asynchronous calls in the components. Adding resources and template components.
    Signed-off-by: Reason <reason@exratione.com>

--- example/simpleExample.js
+++ example/simpleExample.js
@@ -1,11 +1,13 @@
-/**
+/*
  * An example of a thywill HTTP server, using a very simple configuration and a proof of concept application.
  * All this does is echo messages submitted back to the client.
+ * 
+ * When running, navigate to /thywill on the server to see it in action.
  */
 
 var http = require("http");
 var thywill = require("thywill");
-var echoAppClass = require("../lib/apps/echo");
+var EchoAppClass = require("../lib/apps/echo");
 
 /*
  * Start up an HTTP server.
@@ -19,9 +21,13 @@ server.listen(80);
 /*
  * Then define your application object and callback function:
  */
-var echoApp = new echoAppClass("my echo application");
-var callback = function() { 
-  console.log("thywill.js is ready.");
+var echoApp = new EchoAppClass("my echo application");
+var callback = function(error) { 
+  if( error ) {
+    console.log("Error while launching thywill.js: " + error);
+  } else {
+    console.log("thywill.js is ready.");
+  };
 };
 
 /*
@@ -36,6 +42,8 @@ thywill.configureFromFile();
  * We can either pass thywill.startup() a callback function that will be invoked when startup is complete,
  * with an argument that is a reference to the thywill object for convenience, or we can listen for the 
  * "thywill.ready" event elsewhere in our code. Here we are passing a callback.
+ * 
+ * The callback has the form function(error), where error == null on a successful startup.
  */
 thywill.startup(server, echoApp, callback);
 
